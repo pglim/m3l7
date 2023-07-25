d87a7c9cd362920d9a141f14b72cb64b0ff3d313
pglim <limpohguan@yahoo.com>
2023-07-25 21:11:00 +0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
-terraform {
-  backend "s3" {
-    bucket = "sctp-ce2-tfstate-bkt"
-    region = "ap-southeast-1"
-  }
-}
+#terraform {
+ # backend "s3" {
+  #  bucket = "sctp-ce2-tfstate-bkt"
+   # region = "ap-southeast-1"
+  #}
+#}
