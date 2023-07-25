0bd6e1186072b4a4b3c03ac8858b1bb2cb053d1a
pglim <limpohguan@yahoo.com>
2023-07-25 21:04:02 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 
   queue {
     queue_arn     = aws_sqs_queue.queue.arn
-    events        = ["s3:ObjectCreated:*"]
+    events        = ["s3:ObjectCreated:*","s3:ObjectCreated:*" ]
     filter_suffix = ".log"
   }
 }
