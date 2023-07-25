c655901087606472c37f3a384b1b164d6ad3d583
pglim <limpohguan@yahoo.com>
2023-07-25 20:42:46 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_sqs_queue" "queue" {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "your-bucket-name"
+  bucket = "pglim360tf-bucket-name"
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
