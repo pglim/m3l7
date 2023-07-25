56acad09a7ad9f0592fea1334f73faa240695320
pglim <limpohguan@yahoo.com>
2023-07-25 21:07:32 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ data "aws_iam_policy_document" "queue" {
 
     principals {
       type        = "Service"
-      identifiers = ["s3.amazonaws.com*"]
+      identifiers = ["s3.amazonaws.com"]
     }
 
     actions   = ["sqs:SendMessage"]
